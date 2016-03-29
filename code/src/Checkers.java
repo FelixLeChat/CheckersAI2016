778b3c79fd940ae72ca4c3c2cf1d47cb4d4c0416
Konstantin <fedorov.konstantin@gmail.com>
2016-03-29 16:31:41 -0400

    Code initial

---
+++ code/src/Checkers.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASHISH
+ */
+public class Checkers {
+    public static void main(String args[]){        
+        Game game = new Game();
+        game.PlayGame();        
+    }    
+}
